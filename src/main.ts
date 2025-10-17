f4e7c898e9b80e8505ad52ea9bc340053abb9f2e
Daler <isoev9733@gmail.com>
2025-10-17 23:05:37 +0400

    Правка по uploads

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as express from 'express';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use('/uploads', express.static(join(process.cwd(), 'uploads')));
+
   app.enableCors({
     origin: ['http://localhost:4200'],
     credentials: true,
